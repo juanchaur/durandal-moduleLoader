ace79acfc30c2834f95e83825f7b320fef8634c8
Juan Chaur <juan.chaur89@gmail.com>
2015-09-15 21:43:05 +0200

    Fixing some code standards in the specs

--- DurandalAppStarter/webapp/test/specs/dev/welcome.spec.js
+++ DurandalAppStarter/webapp/test/specs/dev/welcome.spec.js
@@ -1,9 +1,10 @@
-define( [ 'module!welcome' ], function ( Welcome ) {
-    
-    /*global jasmine, describe, beforeEach, it, expect, require */
+/*global jasmine, describe, beforeEach, it, expect, require */
+define( [ 'module!welcome' ], function(Welcome) {
+
+	'use strict';
+
     describe('[Module: Welcome]: Suite to test the module Welcome >', function() {
-        "use strict";
-    
+
         //var Welcome = require('viewmodels/welcome');
 
         it('should be a constructor function', function() {
@@ -14,40 +15,40 @@ define( [ 'module!welcome' ], function ( Welcome ) {
         describe('Testing the instance of Welcome', function() {
             var oConstructor = new Welcome();
 
-            it('should have a "displayName" property', function() {
+            it('should have a \'displayName\' property', function() {
                 expect( oConstructor.displayName).toBeDefined();
             });
 
-            it('should have a "description" property', function() {
+            it('should have a \'description\' property', function() {
                 expect( oConstructor.description).toBeDefined();
             });
 
-            it('should have a "features" property', function() {
+            it('should have a \'features\' property', function() {
                 expect( oConstructor.features).toBeDefined();
             });
 
             it('features should be of type Array', function(){
-                expect( oConstructor.features.constructor.name).toEqual("Array");
+                expect( oConstructor.features.constructor.name).toEqual('Array');
                 expect( oConstructor.features.length).toBeDefined();
             });
 
-            it('should have a "counter" property', function() {
+            it('should have a \'counter\' property', function() {
                 expect( oConstructor.counter).toBeDefined();
             });
 
-            it('"counter" property should be a number', function() {
-                expect( typeof oConstructor.counter).toEqual("number");
+            it('\'counter\' property should be a number', function() {
+                expect( typeof oConstructor.counter).toEqual('number');
             });
 
-            it('should have a "anotherFeatures" property', function() {
+            it('should have a \'anotherFeatures\' property', function() {
                 expect( oConstructor.anotherFeatures).toBeDefined();
             });
 
             it('features should be of type Array', function(){
-                expect( oConstructor.anotherFeatures.constructor.name).toEqual("Array");
+                expect( oConstructor.anotherFeatures.constructor.name).toEqual('Array');
                 expect( oConstructor.anotherFeatures.length).toBeDefined();
             });
         });
-        
     });
+
 });
